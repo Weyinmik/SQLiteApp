ab46ef8d42346d78f2ff45f1a4c472cfea195ba0
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-04-09 00:19:45 +0200

    initial commit

--- app/src/main/java/io/github/smarterme/sqliteapp/DatabaseHelper.java
+++ app/src/main/java/io/github/smarterme/sqliteapp/DatabaseHelper.java
@@ -6,7 +6,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "student.db";
-
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "FIRSTNAME";
+    public static final String COL_3 = "SURNAME";
+    public static final String COL_4 = "MARKS";
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
